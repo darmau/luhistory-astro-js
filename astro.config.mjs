9c6e571163139af9f3f070c18ba3c0f3a617ac61
李大毛 <dev@darmau.design>
2024-01-06 15:30:32 +0800

    feat: add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
-import { defineConfig } from 'astro/config';
-
+import {defineConfig} from 'astro/config';
 import {sanityIntegration} from "@sanity/astro";
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanityIntegration({
-    projectId: 'aemgaomh',
-    dataset: 'production',
-    apiVersion: '2022-03-07',
-    useCdn: false
-  }), react()]
+  integrations: [
+    sanityIntegration({
+      projectId: 'aemgaomh',
+      dataset: 'production',
+      apiVersion: '2022-03-07',
+      useCdn: false
+    }),
+    react(),
+    tailwind()]
 });
