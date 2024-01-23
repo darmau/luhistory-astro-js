9dd5991836468ea90687528492bc3a034c91ca50
李大毛 <github@darmau.co>
2024-01-23 21:17:49 +0800

    style: 图片浏览器样式 (#30)

---
+++ src/components/react-icon/ArrowRight.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+const ArrowRight = ({ width = '48', height = '48' }) => (
+    <svg xmlns = "http://www.w3.org/2000/svg" width = {width} height = {height} viewBox = "0 0 48 48" fill = "none">
+      <g clip-path = "url(#clip0_147_656)">
+        <path
+            d = "M7.5 24H40.5" stroke = "#171717" stroke-width = "2" stroke-linecap = "round" stroke-linejoin = "round"
+        />
+        <path
+            d = "M27 10.5L40.5 24L27 37.5" stroke = "#171717" stroke-width = "2" stroke-linecap = "round"
+            stroke-linejoin = "round"
+        />
+      </g>
+    </svg>
+);
+
+export default ArrowRight;
