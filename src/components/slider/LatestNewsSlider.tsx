584d16ffdecfd9d232fb25d252633a779f39532f
李大毛 <github@darmau.co>
2024-01-31 19:59:03 +0800

    style: latest news section

--- src/components/slider/LatestNewsSlider.tsx
+++ src/components/slider/LatestNewsSlider.tsx
@@ -50,7 +50,7 @@ export default ({news}) => {
         {news.map((item, index) => (
             <SwiperSlide>
               <a href={`/archive/detail/${item.slug}`}
-                 className = "relative bg-gray-50 h-[240px] md:h-[544px] flex flex-col justify-end p-4 md:p-10"
+                 className = "relative bg-gray-50 h-[240px] sm:h-[360px] md:h-[544px] flex flex-col justify-end p-4 md:p-10"
               >
                 {item.cover && (
                     <>
