f7e1eb54afd1066094e09cbf4fe7db7627c174b2
李大毛 <github@darmau.co>
2024-03-10 19:19:57 +0800

    refactor: change archive to collection

--- src/data/ArchiveCategory.js
+++ src/data/ArchiveCategory.js
@@ -1,8 +1,9 @@
 const Category = [
-  "historical-data",
-  "interview",
-  "preface",
-  "writing",
+  {name: "Interview", slug: "interview"},
+  {name: "Conversation", slug: "conversation"},
+  {name: "Lecture & Talk", slug: "lecture-talk"},
+  {name: "Preface", slug: "preface"},
+  {name: "Writing", slug: "writing"},
 ]
 
 export default Category
