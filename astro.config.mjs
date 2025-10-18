dacca9fb27550a50b9c2ce2f1fb2cb7f4ec8d889
李大毛 <github@darmau.co>
2025-10-18 15:39:18 +0900

    Remove Partytown integration and enhance Google Analytics tracking in Layout.astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import {defineConfig} from 'astro/config';
 import {sanityIntegration} from "@sanity/astro";
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
-import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -14,9 +13,6 @@ export default defineConfig({
       dataset: 'production',
       apiVersion: '2022-03-07',
       useCdn: false
-    }),
-    partytown({
-      config: { forward: ["dataLayer.push"] },
     }),
     react(),
     tailwind(),
